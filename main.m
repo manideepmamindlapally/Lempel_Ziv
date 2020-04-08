890da57d80d2331a4a120a194a8846e6d2c24d4a
Manideep Mamindlapally <32261157+manideepyx@users.noreply.github.com>
2020-04-08 18:20:30 +0530

    add comments and semantics

--- main.m
+++ main.m
@@ -3,10 +3,12 @@ M = size(X,2);
 seq_length = 1000000;
 DICTIONARY_SIZE = 10000;
 
+%%======================= Generation of the PTM =========================
 % T = markov_PTM_generate(M);
 % save('TRANSITION_PROB.mat', 'T');
 load( 'TRANSITION_PROB.mat', 'T');
 
+%%======================== Generation of the Markov sequence ===========
 % p_0 = ones(1, M)*1/M;
 % seq = markov_sequence_generate(X, T, p_0, seq_length);
 % dlmwrite('data.txt' , seq, 'delimiter', '');
@@ -14,6 +16,10 @@ fID = fopen('data.txt','r');
 seq = fscanf(fID,'%c');
 fclose(fID);
 
-[lempel_code, lempel_code_length] = lempel_ziv_encode(seq, DICTIONARY_SIZE, X);
+%%======================== Encoding using Lempel Zev ===================
+% [lempel_code, lempel_code_length] = lempel_ziv_encode(seq, DICTIONARY_SIZE, X);
+dlmwrite('lempel_ziv_coded_seq.txt', char(lempel_code(1,1:lempel_code_length)), 'delimiter', '');
 
-[huff_code, huff_code_length] = huffman_markov(seq,X,T);
+%%======================== Encoding using Huffman =======================
+% [huff_code, huff_code_length] = huffman_markov(seq,X,T);
+dlmwrite('huffman_markov_coded_seq.txt', char(huff_code(1,1:huff_code_length)), 'delimiter', '');
